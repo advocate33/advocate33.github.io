3181837e9320451f41b06cb454a321d3da9a99db
advocate33 <9107775577@mail.ru>
2026-08-21 21:45:03 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -12,7 +12,7 @@
 
    При выходе новой версии приложения поднимите число в CACHE.
 */
-const CACHE = 'crm-advocate-v25';
+const CACHE = 'crm-advocate-v26';
 
 // Базовый адрес каталога, где лежит SW (работает и в подпапке, и в корне)
 const BASE = new URL('./', self.location).pathname;
